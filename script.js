91729f2216248e1087433a10e1359f03d69d36c4
dmr1991 <65431230+dmr1991@users.noreply.github.com>
2023-04-27 19:43:35 -0700

    Responsive

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-let pantalla = document.getElementById(pantalla).ariaValueMax;
+let pantalla = document.getElementById("pantalla").value;
+let acButton = document.getElementById("acBtn");
+let multiplyButton = document.getElementById("multiplyBtn");
+let clearButton = document.getElementById("clearBtn");
+let periodButton = document.getElementById("periodBtn");
+let equalButton = document.getElementById("equalBtn");
+let ptsRight = document.getElementById("parenthesisRight");
+let ptsLeft = document.getElementById("parenthesisLeft");
+let sqrRootButton = document.getElementById("squareRootBtn");
+let divisionButton = document.getElementById("divisionBtn");
+let plusButton = document.getElementById("plusBtn");
+let percentagePENDINGButton = document.getElementById("porcentajePENDING");
+let minusButton = document.getElementById("minusBtn");
+let n0Btn = document.getElementById("nbr0");
+let n1Btn = document.getElementById("nbr1");
+let n2Btn = document.getElementById("nbr2");
+let n3Btn = document.getElementById("nbr3");
+let n4Btn = document.getElementById("nbr4");
+let n5Btn = document.getElementById("nbr5");
+let n6Btn = document.getElementById("nbr6");
+let n7Btn = document.getElementById("nbr7");
+let n8Btn = document.getElementById("nbr8");
+let n9Btn = document.getElementById("nbr9");
+
+function clickTest() {
+  alert("clicked");
+}
+
+
+
+acButton.addEventListener("click", clickTest);
+multiplyButton.addEventListener("click", clickTest);
+clearButton.addEventListener("click", clickTest);
+periodButton.addEventListener("click", clickTest);
+equalButton.addEventListener("click", clickTest);
+ptsRight.addEventListener("click", clickTest);
+ptsLeft.addEventListener("click", clickTest);
+sqrRootButton.addEventListener("click", clickTest);
+divisionButton.addEventListener("click", clickTest);
+plusButton.addEventListener("click", clickTest);
+percentagePENDINGButton.addEventListener("click", clickTest);
+minusButton.addEventListener("click", clickTest);
+
+n0Btn.addEventListener("click", clickTest);
+n1Btn.addEventListener("click", clickTest);
+n2Btn.addEventListener("click", clickTest);
+n3Btn.addEventListener("click", clickTest);
+n4Btn.addEventListener("click", clickTest);
+n5Btn.addEventListener("click", clickTest);
+n6Btn.addEventListener("click", clickTest);
+n7Btn.addEventListener("click", clickTest);
+n8Btn.addEventListener("click", clickTest);
+n9Btn.addEventListener("click", clickTest);
